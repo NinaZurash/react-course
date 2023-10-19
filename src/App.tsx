5659d03d9175e43d3a746c967780fb3a928cf6bf
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-10-19 13:38:24 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Profile from './components/profile'
+
+function App() {
+  return (
+    <>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </>
+  )
+}
+
+export default App
