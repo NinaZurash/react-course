2f78182d7b5a3938bdda1263dac9d508887f05b9
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-10-23 21:19:43 +0200

    choosing-state

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,36 @@
 import { useState } from 'react'
 
-export default function Form() {
-  const [person, setPerson] = useState({
-    name: 'Niki de Saint Phalle',
-    artwork: {
-      title: 'Blue Nana',
-      city: 'Hamburg',
-      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
-    },
+export default function MovingDot() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
   })
-
-  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPerson({
-      ...person,
-      name: e.target.value,
-    })
-  }
-
-  function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        title: e.target.value,
-      },
-    })
-  }
-
-  function handleCityChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        city: e.target.value,
-      },
-    })
-  }
-
-  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        image: e.target.value,
-      },
-    })
-  }
-  console.log('skdjgn')
   return (
-    <>
-      <label>
-        Name:
-        <input value={person.name} onChange={handleNameChange} />
-      </label>
-      <label>
-        Title:
-        <input value={person.artwork.title} onChange={handleTitleChange} />
-      </label>
-      <label>
-        City:
-        <input value={person.artwork.city} onChange={handleCityChange} />
-      </label>
-      <label>
-        Image:
-        <input value={person.artwork.image} onChange={handleImageChange} />
-      </label>
-      <p>
-        <i>{person.artwork.title}</i>
-        {' by '}
-        {person.name}
-        <br />
-        (located in {person.artwork.city})
-      </p>
-      <img src={person.artwork.image} alt={person.artwork.title} />
-    </>
+    <div
+      onPointerMove={(e) => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY,
+        })
+      }}
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'red',
+          borderRadius: '50%',
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          left: -10,
+          top: -10,
+          width: 20,
+          height: 20,
+        }}
+      />
+    </div>
   )
 }
